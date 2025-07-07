1461cd68e055db70e5d3a86461779bc98733201c
Mats Macke <61540264+MATSMACKE@users.noreply.github.com>
2025-07-07 10:32:17 +0200

    Implemented Taylor tables and matrix inverting

--- discreet-common/src/algebra.rs
+++ discreet-common/src/algebra.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::{collections::HashMap, iter::repeat, vec::IntoIter};
 
 use crate::taylor::DerivativeApproximations;
 
@@ -33,6 +33,23 @@ pub enum Expression {
 }
 
 impl Expression {
+    pub fn list_required_derivatives(&self) -> Vec<(Variable, usize)> {
+        match self {
+            Self::Derivative(v, o) => vec![(*v, *o)],
+            Self::Sum(vec) | Self::Prod(vec) => {
+                let mut items = Vec::new();
+                for item in vec {
+                    let mut req = item.list_required_derivatives();
+                    items.append(&mut req);
+                }
+                items
+            }
+            Self::CrossDerivative(_) => todo!(),
+            Self::Negate(e) | Self::Reciprocal(e) => e.list_required_derivatives(),
+            _ => vec![],
+        }
+    }
+
     pub fn substitute<F: Fn(&Self) -> Option<Self>>(self, func: &F) -> Self {
         match func(&self) {
             Some(expr) => expr,
@@ -57,7 +74,7 @@ impl Expression {
 }
 
 /// An expression in terms of values on the mesh
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, PartialEq)]
 pub enum MeshExpr {
     AtOffset(isize, isize),
     Prod(Vec<MeshExpr>),
@@ -132,11 +149,100 @@ impl MeshExpr {
     }
 }
 
+#[derive(Clone, Debug, PartialEq)]
+pub struct SquareMat {
+    size: usize,
+    /// Stored as cols
+    vals: Vec<f64>,
+}
+
+impl SquareMat {
+    pub fn new(cols: Vec<Vec<f64>>) -> Self {
+        let size = cols.len();
+
+        let vals: Vec<_> = cols.into_iter().flat_map(Vec::into_iter).collect();
+
+        Self { size, vals }
+    }
+
+    pub fn ident(size: usize) -> Self {
+        let mut vals: Vec<f64> = repeat(0.).take(size * size).collect();
+
+        for i in 0..size {
+            let idx = Self::get_index(size, i, i);
+
+            vals[idx] = 1.;
+        }
+
+        Self { size, vals }
+    }
+
+    pub fn invert(mut self) -> Self {
+        let mut inverse = Self::ident(self.size);
+
+        for i in 0..self.size {
+            inverse.row_scale(i, 1. / self.get_at(i, i));
+            self.row_scale(i, 1. / self.get_at(i, i));
+
+            for j in 0..self.size {
+                if i == j {
+                    continue;
+                }
+                inverse.row_sub(j, i, self.get_at(j, i));
+                self.row_sub(j, i, self.get_at(j, i));
+            }
+        }
+
+        inverse
+    }
+
+    pub fn row_scale(&mut self, target_row: usize, factor: f64) {
+        for i in 0..self.size {
+            let target_idx = Self::get_index(self.size, target_row, i);
+
+            self.vals[target_idx] *= factor
+        }
+    }
+
+    pub fn row_sub(&mut self, target_row: usize, source_row: usize, factor: f64) {
+        for i in 0..self.size {
+            let source_idx = Self::get_index(self.size, source_row, i);
+            let target_idx = Self::get_index(self.size, target_row, i);
+
+            self.vals[target_idx] -= factor * self.vals[source_idx]
+        }
+    }
+
+    pub fn get_at(&self, row: usize, col: usize) -> f64 {
+        let target_idx = Self::get_index(self.size, row, col);
+        self.vals[target_idx]
+    }
+
+    pub fn get_index(size: usize, row: usize, col: usize) -> usize {
+        row + col * size
+    }
+
+    pub fn get_cols(&self) -> Vec<Vec<f64>> {
+        let mut iter = self.vals.iter();
+
+        let mut cols = Vec::with_capacity(self.size);
+        for _ in 0..self.size {
+            let mut col = Vec::with_capacity(self.size);
+            for _ in 0..self.size {
+                col.push(*iter.next().unwrap());
+            }
+            cols.push(col);
+        }
+
+        cols
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::algebra::Variable;
 
-    use super::Expression;
+    use super::{Expression, SquareMat};
 
     #[test]
     fn substituting_derivatives() {
@@ -164,5 +270,39 @@ mod test {
             e,
             Expression::Prod(vec![Expression::Constant(0.54), Expression::Constant(42.0)])
         );
+    }
+
+    #[test]
+    fn matrix_inv() {
+        let mat = SquareMat::new(vec![
+            vec![1., -0.5, 1.],
+            vec![1., 0., 1.],
+            vec![0.5, 1.5, 1.],
+        ]);
+
+        let inverse = mat.invert();
+
+        assert_eq!(
+            inverse,
+            SquareMat::new(vec![
+                vec![-6., 8., -2.],
+                vec![-2., 2., 0.],
+                vec![6., -7., 2.]
+            ])
+        )
+    }
+
+    #[test]
+    fn matrix_get_cols() {
+        let mat = SquareMat::new(vec![
+            vec![1., -0.5, 1.],
+            vec![1., 0., 1.],
+            vec![0.5, 1.5, 1.],
+        ]);
+
+        assert_eq!(
+            mat.get_cols(),
+            vec![vec![1., -0.5, 1.], vec![1., 0., 1.], vec![0.5, 1.5, 1.],]
+        )
     }
 }
--- discreet-common/src/taylor.rs
+++ discreet-common/src/taylor.rs
@@ -1,33 +1,129 @@
-use std::collections::{BTreeMap, HashMap};
+use std::collections::HashMap;
 
-use crate::algebra::{MeshExpr, Variable};
+use crate::algebra::{MeshExpr, SquareMat, Variable};
+
+fn fact(n: usize) -> usize {
+    (1..=n).product()
+}
 
 pub struct TaylorTable {
+    /// Columns of the inverted Taylor table matrix. Its columns are simply
+    /// the coefficients for each order derivative, zero-indexed.
     cols: Vec<Vec<f64>>,
+    variable: Variable,
+    stencil: Vec<isize>,
 }
 
 impl TaylorTable {
     pub fn new(stencil: &[(isize, isize)], variable: Variable) -> Self {
-        let filter_fn: fn(&(isize, isize)) -> bool = match variable {
-            Variable::X => |(_, y)| *y == 0,
-            Variable::Y => |(x, _)| *x == 0,
+        let filter_fn: fn((isize, isize)) -> Option<isize> = match variable {
+            Variable::X => |(x, y)| if y == 0 { Some(x) } else { None },
+            Variable::Y => |(x, y)| if x == 0 { Some(y) } else { None },
         };
 
-        let stencil: Vec<(isize, isize)> = stencil.iter().copied().filter(filter_fn).collect();
+        let stencil: Vec<isize> = stencil.iter().copied().filter_map(filter_fn).collect();
 
         let size = stencil.len();
         let mut cols = Vec::with_capacity(size);
-        cols.resize_with(size, || {
-            let mut row = Vec::with_capacity(size);
-            row.resize_with(size, || 0.0);
-            row
-        });
-        Self { cols }
+
+        for offset in &stencil {
+            let mut col = Vec::with_capacity(size);
+
+            for j in 0..size {
+                let item = offset.pow(j as u32) as f64 / (fact(j) as f64);
+                col.push(item);
+            }
+
+            cols.push(col);
+        }
+
+        let mat = SquareMat::new(cols);
+        let inv = mat.invert();
+        let cols = inv.get_cols();
+
+        Self {
+            cols,
+            variable,
+            stencil,
+        }
     }
 
-    pub fn get_scheme(derivative_order: usize) -> MeshExpr {
-        todo!()
+    pub fn get_scheme(&self, derivative_order: usize) -> MeshExpr {
+        let col = &self.cols[derivative_order];
+        let size = self.cols.len();
+
+        let mut terms = Vec::with_capacity(size);
+
+        for i in 0..size {
+            let coeff = col[i];
+            if coeff == 0. {
+                continue;
+            }
+
+            let stencil_value = self.stencil[i];
+
+            let (i, j) = match self.variable {
+                Variable::X => (stencil_value, 0),
+                Variable::Y => (0, stencil_value),
+            };
+
+            terms.push(MeshExpr::Prod(vec![
+                MeshExpr::Constant(coeff),
+                MeshExpr::AtOffset(i, j),
+            ]));
+        }
+
+        MeshExpr::Sum(terms)
     }
 }
 
 pub type DerivativeApproximations = HashMap<(Variable, usize), MeshExpr>;
+
+#[cfg(test)]
+mod test {
+    use crate::{
+        algebra::{MeshExpr, Variable},
+        taylor::fact,
+    };
+
+    use super::TaylorTable;
+
+    #[test]
+    fn fact_test() {
+        assert_eq!(fact(0), 1);
+        assert_eq!(fact(1), 1);
+        assert_eq!(fact(2), 2);
+        assert_eq!(fact(3), 6);
+        assert_eq!(fact(4), 24);
+    }
+
+    #[test]
+    fn one_sided_diff_3_nodes() {
+        let stencil = vec![(-2, 0), (0, 1), (-1, 0), (0, 0)];
+        let table = TaylorTable::new(stencil.as_slice(), Variable::X);
+
+        assert_eq!(
+            table.cols,
+            vec![vec![0., 0., 1.], vec![0.5, -2., 1.5], vec![1., -2., 1.]]
+        );
+
+        assert_eq!(table.stencil, vec![-2, -1, 0]);
+
+        assert_eq!(table.variable, Variable::X)
+    }
+
+    #[test]
+    fn forward_diff() {
+        let stencil = vec![(0, 0), (1, 0)];
+        let table = TaylorTable::new(stencil.as_slice(), Variable::X);
+        let scheme = table.get_scheme(1);
+
+        assert_eq!(
+            scheme,
+            MeshExpr::Sum(vec![
+                MeshExpr::Prod(vec![MeshExpr::Constant(-1.0), MeshExpr::AtOffset(0, 0)]),
+                MeshExpr::Prod(vec![MeshExpr::Constant(1.0), MeshExpr::AtOffset(1, 0)])
+            ])
+        )
+    }
+}
